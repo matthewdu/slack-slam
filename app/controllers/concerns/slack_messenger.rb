70ce40205b2d2760e45b3618ac905df5266ca8a8
Angel Gao <angel_gao@live.ca>
2015-03-08 01:22:17 -0800

    Moved command logic to slack_messenger

--- app/controllers/concerns/slack_messenger.rb
+++ app/controllers/concerns/slack_messenger.rb
@@ -1,6 +1,6 @@
 module SlackMessenger extend ActiveSupport::Concern
   def handle_message(request)
-    words = tokenize_message(request[:text].downcase)
+    words = tokenize_message(request[:slack_text].downcase)
 
     # Don't do anything if "slam! is not in message"
     return if words.nil?
@@ -18,34 +18,61 @@ module SlackMessenger extend ActiveSupport::Concern
   end
 
   def process_command(request, words)
-    case words[1]
+    team = Team.find_by(:slack_team_id => request[:slack_team_id])
+    user = team.users.find_by(:slack_user_id => request[:slack_user_id])
+
+    case words.index(1)
     when "add"
-      # angel code
+      key = words.index(2)
+      value = words.index(3)
+      if (key && value)
+        if user.commands.create(:key => key, :value => value)
+          byebug
+          post_message(request, "#{key} has been mapped to #{value}") #change to update
+        else
+          #ERROR
+        end
+      end
     when "list"
-      # angel code
+      list_commands = user.commands
+        message = ""
+        list_commands.each do |command|
+          message += "#{command[:key]}: #{command[:value]}\n"
+        end
+        post_message(request, message)
     else
-      post_message(request, "Sorry, try using \"slam! add [key] [value]\" or \"slam! list\"")
+      if words.index(1)
+        key = words.index(1)
+        value = user.commands.find_by(:key => key)
+        if value
+          post_message(request, value)
+        else
+          post_message(request, "No mapping found for #{key}")
+        end
+      else
+        update_message(request, "No command entered.")
+      end
     end
   end
 
   def post_message(request, message)
-    user = User.find_by(:slack_user_id => request[:user_id])
+    user = User.find_by(:slack_user_id => request[:slack_user_id])
     RestClient.post(
       'https://slack.com/api/chat.postMessage',
       :token   => user.access_token,
-      :channel => request[:channel_id],
+      :channel => request[:slack_channel_id],
       :text    => message,
       :as_user => true
     )
   end
 
   def update_message(request, message)
-    user = User.find_by(:slack_user_id => request[:user_id])
+    user = User.find_by(:slack_user_id => request[:slack_user_id])
     RestClient.post(
       'https://slack.com/api/chat.update',
       :token   => user.access_token,
-      :channel => request[:channel_id],
-      :ts      => request[:time_stamp],
+      :channel => request[:slack_channel_id],
+      :ts      => request[:timestamp],
       :text    => message
     )
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   belongs_to :team
+  has_many :commands
 
   validates :slack_name, :slack_user_id, :team_id, :access_token, :presence => true
 end
